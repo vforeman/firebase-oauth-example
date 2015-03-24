da13d36e7a477759f93aadd402c673a56a55e986
victor foreman <vforeman@scs.howard.edu>
2015-03-24 14:27:20 -0400

    logout functionality and basic routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,14 @@
   document.addEventListener('polymer-ready', function () {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+    var app = document.querySelector("#app");
+    app.appName = 'firebase oauth example';
+    app.page = 'home';
+    console.log(app);
+    
   });
 
-  
+
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
